e1a2ced561e20c8fffdcca4ae8785cdcc0477c3f
nasturtus <nasturtus@gmail.com>
2018-03-06 23:45:11 +0800

    Debugged script.js

--- script.js
+++ script.js
@@ -1,36 +1,25 @@
 var tasks = ["buy milk", "eat dinner", "nail javascript"];
 
-function createAndAppendToDoList() {
+function initialiseToDoList() {
   // clear existing display of ToDos
-
   document.getElementById("todo-list").textContent = "";
 
   // render updated ToDo List
-  
   tasks.forEach(element => {
     var li = document.createElement("li");
     li.textContent = element;
-    li.className = "pending";
+    li.className = 'pending'
 
-    li.addEventListener("click", function() {
-      if (li.className === "pending") {
-        li.className = "done";
-      } else {
-        li.className = "pending";
-      }
-    });
-
+    createAndAddEventListener(li);
     var ul = document.getElementById("todo-list");
     ul.appendChild(li);
   });
 }
 
-createAndAppendToDoList();
+initialiseToDoList();
 
-// handle enter or 'add to do' button event
-
+// handle enter or 'Add To Do' button event
 var btn = document.getElementsByTagName("button")[0];
-console.log(btn);
 
 btn.addEventListener("click", function(event) {
   event.preventDefault();
@@ -38,5 +27,24 @@ btn.addEventListener("click", function(event) {
   var toDoString = formInput.value;
   formInput.value = "";
   tasks.push(toDoString);
-  createAndAppendToDoList();
+  document.getElementsByTagName("form")[0].reset();
+
+  var li = document.createElement("li");
+  li.textContent = toDoString;
+  li.className = 'pending'
+
+  createAndAddEventListener(li);
+
+  var ul = document.getElementById("todo-list");
+  ul.appendChild(li);
 });
+
+function createAndAddEventListener(element) {
+  element.addEventListener("click", function() {
+    if (element.className === "pending") {
+      element.className = "done";
+    } else {
+      element.className = "pending";
+    }
+  });
+}
